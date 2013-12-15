5834a812ae1db73a57866b2c9fa296fb789e2792
sconine <sconine@users.noreply.github.com>
2013-12-15 09:54:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo 'hello world';
+require 'vendor/autoload.php';
+
+use Aws\Common\Aws;
+
+// Create a service building using shared credentials for each service
+$aws = Aws::factory(array(
+    'key'    => 'your-aws-access-key-id',
+    'secret' => 'your-aws-secret-access-key'
+   // ,'region' => 'us-west-2' -> not needed for S3
+));
+
+$aws = Aws::factory('/path/to/my_config.json');
+$s3 = $aws->get('s3');
+
 ?>
