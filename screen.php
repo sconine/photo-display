374c354d3ccb0de3131300ff9c773e9ba698ae48
sconine <sconine@users.noreply.github.com>
2013-12-18 03:12:26 -0500

    Update screen.php

--- screen.php
+++ screen.php
@@ -6,14 +6,23 @@
 <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 </head>
 <body>
-<div class="my_media">Media will go here</div>
-
+ 
+<p>
+  <b>Click</b> to change the <span id="tag">html</span>
+</p>
+<p>
+  to a <span id="text">text</span> node.
+</p>
+<p>
+  This <button name="nada">button</button> does nothing.
+</p>
+ 
 <script>
-
-//setTimeout(alert('timeed alert'), 500000);
-var htmlString = $("my_media").html();
-alert(htmlString);
-
+$( "p" ).click(function() {
+  var htmlString = $( this ).html();
+  $( this ).text( htmlString );
+});
 </script>
-
+ 
 </body>
+</html>
