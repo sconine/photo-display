cd6947b5d56a170ff3ff5a88d5b43733ef29270c
sconine <sconine@users.noreply.github.com>
2013-12-18 02:37:43 -0500

    Create drive_screen.php

---
+++ drive_screen.php
@@ -0,0 +1,14 @@
+<?php
+// This script is responsible for servicing the Ajax requests 
+// the browsers running on the "screen" display devices make
+//
+// returns: JSON that details:
+//    Next URL to show
+//    New Media Type
+//    Duration to Show Media
+//    If a caption should be shown
+
+
+echo '{json data goes here'};
+
+?>
