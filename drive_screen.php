4e34ce1cc66f94c05c3287aa0a3ee47585aa25b9
sconine <sconine@users.noreply.github.com>
2013-12-18 03:34:54 -0500

    Update drive_screen.php

--- drive_screen.php
+++ drive_screen.php
@@ -8,7 +8,8 @@
 //    Duration to Show Media
 //    If a caption should be shown
 
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 
-echo '{json data goes here'};
+echo json_encode($arr);
 
 ?>
