010bef6ceb3720db91ee5ea80194e120b2e63711
sconine <sconine@users.noreply.github.com>
2013-12-18 03:05:16 -0500

    Update screen.js

--- screen.js
+++ screen.js
@@ -1,10 +1,10 @@
 //JQuery Javascript to drive the screen
 
 //$( "media" ).delay( 8000 );
-$( "media" ).html('Hi There');
+$( "my_media" ).html('Hi There');
 
 //setTimeout(alert('timeed alert'), 500000);
 //$( "media" ).text('Hi There World');
 
-alert($( "media" ).html);
+alert($( "my_media" ).html);
 
