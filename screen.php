5f57fb9d134ce00d8072e52f42773f9fbbd68761
sconine <sconine@users.noreply.github.com>
2013-12-18 03:11:45 -0500

    Update screen.php

--- screen.php
+++ screen.php
@@ -4,8 +4,16 @@
 <html>
 <head><Title>Photo Display Screen</title>
 <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="screen.js"></script>
 </head>
 <body>
 <div class="my_media">Media will go here</div>
+
+<script>
+
+//setTimeout(alert('timeed alert'), 500000);
+var htmlString = $("my_media").html();
+alert(htmlString);
+
+</script>
+
 </body>
