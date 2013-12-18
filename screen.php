64ee553ed169bfb3572abd5fd5f7cba1638f8557
sconine <sconine@users.noreply.github.com>
2013-12-18 02:41:37 -0500

    Create screen.php

---
+++ screen.php
@@ -0,0 +1,11 @@
+<?php
+// Script that browser that drives the "screens" initially loads
+?>
+<html>
+<head><Title>Photo Display Screen</title>
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="screen.js"></script>
+</head>
+<body>
+<div id="media">Media will go here</div>
+</body>
