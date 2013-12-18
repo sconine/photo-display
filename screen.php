a4b872609ca885f42af8d5da71fbf9d9053d8324
sconine <sconine@users.noreply.github.com>
2013-12-18 03:26:35 -0500

    Update screen.php

--- screen.php
+++ screen.php
@@ -15,7 +15,8 @@
 
 function change_media(cnt) {
  $( "#my_media" ).text( 'Media #' + cnt );
- //setTimeout(change_media(cnt++), 2000);
+ cnt++;
+ setTimeout(function(){change_media(cnt);}, 2000);
 }
 
 change_media(0);
