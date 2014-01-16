1e6dc348a8cf5e6494ec78ea1b8aee100791f76a
sconine <sconine@users.noreply.github.com>
2014-01-15 19:55:50 -0500

    Update send_media.php

--- php/send_media.php
+++ php/send_media.php
@@ -6,42 +6,53 @@ require 'vendor/autoload.php';
 
 use Aws\Common\Aws;
 
-// You'll need ot edit this with your config
-$aws = Aws::factory('/usr/www/html/photo-display/amz_config.json');
+// You'll need to edit this with your config
+$aws = Aws::factory('/usr/www/html/photo-display/php/amz_config.json');
 $client = $aws->get('s3');
 
-$result = $client->listBuckets();
+// Set the bucket for where media is stored
+$bucket = 'SConine_Photos';
+if (isset($_REQUEST['media_id'])) {
+    // Get an object using the getObject operation
+    $result = $client->getObject(array(
+        'Bucket' => $bucket,
+        'Key'    => $_REQUEST['media_id']
+    ));
 
-foreach ($result['Buckets'] as $bucket) {
+    // Deal with consol calls vs calls through a web server
+    if (isset($_SERVER['HTTP_HOST'])) {
+        $f_ext = strtolower(substr($_REQUEST['media_id'], -3));
+        if ($f_ext == 'mov') {
+            header('Content-type: video/quicktime');
+        } elseif ($f_ext == 'gif') {
+            header('Content-type: image/gif');
+        } else {
+            header('Content-type: image/jpg');
+        }
+        echo $result['Body'];
+    } else {
+        echo "Got the body, but did not stream to concole\n";
+    }
+}
+
+
+
+// Code to see buckets (useful for debug)
+//$result = $client->listBuckets();
+//foreach ($result['Buckets'] as $bucket) {
     // Each Bucket value will contain a Name and CreationDate
     //echo "{$bucket['Name']} - {$bucket['CreationDate']}\n";
-}
+//}
 
-$bucket = 'SConine_Photos';
+// Method to go into a specific bucket with a filter
+//$bucket = 'SConine_Photos';
+//$iterator = $client->getIterator('ListObjects', array(
+//    'Bucket' => $bucket,
+//    'Prefix' => 'Dec-2005'
+//));
 
-$iterator = $client->getIterator('ListObjects', array(
-    'Bucket' => $bucket,
-    'Prefix' => 'Dec-2005'
-));
-
-foreach ($iterator as $object) {
+//foreach ($iterator as $object) {
     //echo $object['Key'] . "\n";
-}
-
-// Get an object using the getObject operation
-$result = $client->getObject(array(
-    'Bucket' => $bucket,
-    'Key'    => 'Oct-2012/DSCN2599.MOV'
-));
-
-
-if (isset($_SERVER['HTTP_HOST'])) {
-    // This works sending to Chrome
-    header('Content-type: video/quicktime');
-    echo $result['Body'];
-} else {
-    echo "Got the body, but did not stream to concole\n";
-}
-
+//}
 
 ?>
