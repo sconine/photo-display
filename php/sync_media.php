3aa5467fad5f32a52b30af25fd32cae416823c13
sconine <sconine@users.noreply.github.com>
2014-01-15 20:21:20 -0500

    Update sync_media.php

--- php/sync_media.php
+++ php/sync_media.php
@@ -1,6 +1,5 @@
 <?php
-// This is currently a sample script that I'm using to understand communication
-// with S3 and Amazon in general
+// Get the media we have stored on S3 and load it into a dynamoDB
 
 require 'vendor/autoload.php';
 
@@ -8,51 +7,53 @@ use Aws\Common\Aws;
 
 // You'll need to edit this with your config
 $aws = Aws::factory('/usr/www/html/photo-display/php/amz_config.json');
-$client = $aws->get('s3');
+$client = $aws->get('DynamoDb');
+$result = $client->listTables();
 
-// Set the bucket for where media is stored
-$bucket = 'SConine_Photos';
-if (isset($_REQUEST['media_id'])) {
-    // Get an object using the getObject operation
-    $result = $client->getObject(array(
-        'Bucket' => $bucket,
-        'Key'    => $_REQUEST['media_id']
-    ));
-
-    // Deal with consol calls vs calls through a web server
-    if (isset($_SERVER['HTTP_HOST'])) {
-        $f_ext = strtolower(substr($_REQUEST['media_id'], -3));
-        if ($f_ext == 'mov') {
-            header('Content-type: video/quicktime');
-        } elseif ($f_ext == 'gif') {
-            header('Content-type: image/gif');
-        } else {
-            header('Content-type: image/jpg');
-        }
-        echo $result['Body'];
-    } else {
-        echo "Got the body, but did not stream to concole\n";
+// TableNames contains an array of table names
+$has_table = false;
+foreach ($result['TableNames'] as $table_name) {
+    if ($table_name == "media_files") {$has_table = true;}
+    if (!if (isset($_SERVER['HTTP_HOST'])) {
+        echo $$table_name . "\n";
     }
 }
 
+// Create table is non-existent
+if (!$has_table ) {
+    $client->createTable(array(
+    'TableName' => 'media_files',
+    'AttributeDefinitions' => array(
+        array(
+            'AttributeName' => 'file_name',
+            'AttributeType' => 'S'
+        ),
+        array(
+            'AttributeName' => 'shown_state',
+            'AttributeType' => 'N'
+        ),
+        array(
+            'AttributeName' => 'shown_on',
+            'AttributeType' => 'SS'
+        )
+    ),
+    'KeySchema' => array(
+        array(
+            'AttributeName' => 'file_name',
+            'KeyType'       => 'HASH'
+        ),
+        array(
+            'AttributeName' => 'shown_state',
+            'KeyType'       => 'RANGE'
+        )
+    ),
+    'ProvisionedThroughput' => array(
+        'ReadCapacityUnits'  => 10,
+        'WriteCapacityUnits' => 20
+    )
+));
+    
+}
 
-
-// Code to see buckets (useful for debug)
-//$result = $client->listBuckets();
-//foreach ($result['Buckets'] as $bucket) {
-    // Each Bucket value will contain a Name and CreationDate
-    //echo "{$bucket['Name']} - {$bucket['CreationDate']}\n";
-//}
-
-// Method to go into a specific bucket with a filter
-//$bucket = 'SConine_Photos';
-//$iterator = $client->getIterator('ListObjects', array(
-//    'Bucket' => $bucket,
-//    'Prefix' => 'Dec-2005'
-//));
-
-//foreach ($iterator as $object) {
-    //echo $object['Key'] . "\n";
-//}
 
 ?>
