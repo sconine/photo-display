4a98a1847d37b1b9b4c119872c2341d665397daf
sconine <sconine@users.noreply.github.com>
2014-02-02 20:40:40 -0500

    Update send_media_queue.php

--- php/send_media_queue.php
+++ php/send_media_queue.php
@@ -33,21 +33,29 @@ if ($debug) {
 
 /////////////////////////////////////////////////
 // We should have been sent a screen_id & region request variables
+// Future functionality:
+// 	See if there are filters for this region/screen
+// 	See if this is video media that needs to be synchronized
 
-// See if there are filters for this region/screen
+$sql = "SELECT media_path FROM media_files WHERE shown=0 ORDER BY rnd_id LIMIT 25;";
+$send_media = query_to_array($sql, &$mysqli);
+
+$usql = '';
+foreach ($send_media as $i=>$media_path) {
+	$usql .= sqlq($media_path,0) . ',';
+}
+
+// Always reset things that were sent but not congfirmed
+$sql = 'UPDATE media_files SET shown=2 WHERE media_path IN (' . $usql . ');';
+if ($debug) {echo "Running: $sql\n";}
+if (!$mysqli->query($sql)) {die("Insert Failed: (" . $mysqli->errno . ") " . $mysqli->error);}
+
+//Send this list of files to the caller
+echo json_encode($send_media);
 
 
 
-// see if this is video media that needs to be synchronized
-
-
-
-
-
-//if ($debug) {echo '<hr>'; var_dump($to_ret);}
-echo json_encode($to_ret);
-
-
+/////////////////////////////////////////////////
 // Query helper functions
 function sqlq($var, $var_type) {
   if ($var_type == 1) {
