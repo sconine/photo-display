cc25714e9bf7a052f5e12a2e42dfbcc8015c44aa
sconine <sconine@users.noreply.github.com>
2014-02-05 04:59:22 -0500

    Update sync_media.php

--- php/sync_media.php
+++ php/sync_media.php
@@ -37,18 +37,6 @@ $sql = 'CREATE TABLE IF NOT EXISTS media_files ('
 if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
 if ($debug) {echo 'media_files table Exists'. "\n";}
 
-// connect to S3 and get a list of files we are storeing
-// Unknown: What is the pratical upper limit to # of files, hoping it is like 1M
-$s3_client = $aws->get('s3');
-
-// Set the bucket for where media is stored and retrive all objects
-// this is what could get to be a big list
-$bucket = 'SConine_Photos';
-$media_iterator = $s3_client->getIterator('ListObjects', array(
-    'Bucket' => $bucket
-    //,'Prefix' => 'Dec-2005'  // this will filter to specific matches
-));
-
 // First see if we've shown everything, if so we'll re-randomize
 $sql = 'SELECT id FROM media_files WHERE shown=0 LIMIT 50;';
 $shown_all = query_to_array($sql, $mysqli);
@@ -65,9 +53,20 @@ $sql = 'UPDATE media_files SET shown=0 WHERE shown=3;';
 if ($debug) {echo "Running: $sql\n";}
 if (!$mysqli->query($sql)) {die("Insert Failed: (" . $mysqli->errno . ") " . $mysqli->error);}
 
+// connect to S3 and get a list of files we are storeing
+// Unknown: What is the pratical upper limit to # of files, hoping it is like 1M
+$s3_client = $aws->get('s3');
+
+// Set the bucket for where media is stored and retrive all objects
+// this is what could get to be a big list
+$bucket = 'SConine_Photos';
+$media_iterator = $s3_client->getIterator('ListObjects', array(
+    'Bucket' => $bucket
+    //,'Prefix' => 'Dec-2005'  // this will filter to specific matches
+));
 
 // Loop through files and add to our local index
-foreach ($iterator as $s3_item) {
+foreach ($media_iterator as $s3_item) {
 	$sql = 'INSERT IGNORE INTO media_files (media_path, rnd_id, shown) VALUES ('
 		. sqlq($object['Key'],0) . ','
 		. '(FLOOR( 1 + RAND( ) *60 )), 0)';
