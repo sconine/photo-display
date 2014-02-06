5a06984134f37302d512e1be24cbb642a45ce254
sconine <sconine@users.noreply.github.com>
2014-02-06 08:57:51 -0500

    Update sync_media.php

--- php/sync_media.php
+++ php/sync_media.php
@@ -67,6 +67,7 @@ $media_iterator = $s3_client->getIterator('ListObjects', array(
 ));
 
 // Loop through files and add to our local index
+// TODO: DELETE files that no longer exist
 foreach ($media_iterator as $s3_item) {
 	$file_path = trim($s3_item['Key']);
 	// don't bother storing folder names
