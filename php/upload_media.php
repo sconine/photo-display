5f055805c5ec6fc47b806248e41223902991dc8b
sconine <sconine@users.noreply.github.com>
2014-02-25 20:28:48 -0500

    Update upload_media.php

--- php/upload_media.php
+++ php/upload_media.php
@@ -74,7 +74,7 @@ foreach ($local_files as $file_path => $i) {
 			// only store the files we care about
 			if ($media_type != '') {
 				// This is a file we'd like to store see if we have already
-				$remote_path = str_replace($file_path, $localpath , '')
+				$remote_path = str_replace($file_path, $localpath , '');
 				if ($remote_files[$remote_path] <> 1) {
 					echo "store: $file_path\n";
 				}
