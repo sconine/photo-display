c788c5963d24c3f878348dd1d9e789ad683cc1f1
sconine <sconine@users.noreply.github.com>
2014-02-25 20:56:54 -0500

    Update upload_media.php

--- php/upload_media.php
+++ php/upload_media.php
@@ -8,7 +8,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     if (isset($_REQUEST['debug'])) {$debug = true;}
 }
 // Load my configuration
-$localpath = "/Volumes/My Pictures/xmas"; // modify with your local folder
+$localpath = "/Volumes/My Pictures/1970-1980"; // modify with your local folder
 $datastring = file_get_contents('/usr/www/html/photo-display/master_config.json');
 $config = json_decode($datastring, true);
 
