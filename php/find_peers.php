b9cc66b958bad5bbb1eaad1c9ea386be74d32b57
conine <sconine@gmail.com>
2014-03-05 20:25:19 -0500

    adding speed control

--- php/find_peers.php
+++ php/find_peers.php
@@ -168,9 +168,9 @@ if (!isset($result['Item']['screen_id']['S'])) {
             'screen_private_ip'    => $screen_private_ip,
             'screen_public_ip'    => $screen_public_ip,
             'screen_last_checkin'    => $time,
-            'screen_active'    => true,
+            'screen_active'    => 1,
             'screen_setting_change_speed' => 5,
-            'screen_setting_movie_override_speed' => true
+            'screen_setting_movie_override_speed' => 1
         )),
         'ReturnConsumedCapacity' => 'TOTAL'
     ));
@@ -222,8 +222,8 @@ foreach ($iterator as $item) {
     $ta['screen_private_ip'] = $item['screen_private_ip']['S'];
     $ta['screen_public_ip'] = $item['screen_public_ip']['S'];
     $ta['screen_active'] = $item['screen_active']['N'];
-    $ta['screen_settings']['change_speed'] = $item['screen_setting_change_speed']['N'];
-    $ta['screen_settings']['movie_override_speed'] = $item['screen_setting_movie_override_speed']['B'];
+    $ta['screen_settings']['change_speed'] = isset($item['screen_setting_change_speed']['N']) ? $item['screen_setting_change_speed']['N'] : 8;
+    $ta['screen_settings']['movie_override_speed'] = isset($item['screen_setting_movie_override_speed']['N']) ? $item['screen_setting_movie_override_speed']['N'] : true;
     $to_ret[] = $ta;
 }
 
