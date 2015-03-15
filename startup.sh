dca8d9b96994ef05041cf20f820bea859104dff4
sconine <sconine@users.noreply.github.com>
2015-03-15 06:40:35 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 cd /usr/www/html/photo-display/node/ && supervisor /usr/www/html/photo-display/node/media_display.js&
 unclutter -idle 2 -root&
 sed -i 's/exit_type\"\:\ \"Crashed/exit_type\"\:\ \"normal/g' /home/conine/.config/google-chrome/Default/Preferences
+xset s off
+xset -dpms
 sleep 5
 google-chrome --kiosk http://localhost:8080/slides
 
