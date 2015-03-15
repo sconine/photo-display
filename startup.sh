1024df3739181221ae96e54102f03e366f1c570b
sconine <sconine@users.noreply.github.com>
2015-03-14 18:17:20 -0600

    Update startup.sh
    added a line that makes sure chrome doesn't display the Google Chrome didn’t shutdown correctly error

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 cd /usr/www/html/photo-display/node/ && supervisor /usr/www/html/photo-display/node/media_display.js&
 unclutter -idle 2 -root&
+sed -i 's/exit_type\"\:\ \"Crashed/exit_type\"\:\ \"normal/g' /home/conine/.config/google-chrome/Default/Preferences
 sleep 5
 google-chrome --kiosk http://localhost:8080/slides
 
